f43f8774bd8f157eaeb8ec839a42f1af973f19ee
Ivan Mauricio Marin Lombo <black_death_metalero@outlook.com>
2023-03-30 09:05:51 -0500

    New Change

---
+++ UnitTestThales/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("UnitTestThales")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("UnitTestThales")>
+<Assembly: AssemblyCopyright("Copyright © HP 2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("2026ce45-bcf0-419a-9afe-88bcea97cea9")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ UnitTestThales/TestEmployee.vb
@@ -0,0 +1,19 @@
+﻿Imports ApiRestThales
+
+<TestClass()>
+Public Class TestEmployee
+    Dim GetEmployeClass As New Cl_Employee
+
+    <TestMethod()>
+    Public Sub TestGetEmployee()
+        Try
+            Dim GetEmployeeModelText As New EmployeeModel
+            Dim retorno As String = GetEmployeClass.InsertEmployee(GetEmployeeModelText)
+            Assert.AreEqual("GUARDADO CON EXITO", retorno)
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+End Class
